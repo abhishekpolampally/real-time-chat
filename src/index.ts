97e22ef11030d13a2a690a03864ea4b110947aed
Abhishek Polampally <abhishek.polampally@broadcom.com>
2024-12-01 20:03:01 +0530

    Updated backend

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,118 @@
-import WebSocket, { WebSocketServer } from "ws";
+import {
+  OutgoingMessage,
+  SupportedMessage as OutgoingSupportedMessages,
+} from "./messages/outgoingMessages";
+import { server as WebSocketServer, connection } from "websocket";
 import http from "http";
+import { UserManager } from "./UserManager";
+import { IncomingMessage, SupportedMessage } from "./messages/incomingMessages";
 
-const server = http.createServer(function (req, res) {
-  res.end("hi there");
+import { InMemoryStore } from "./store/InMemoryStore";
+
+const server = http.createServer(function (request: any, response: any) {
+  console.log(new Date() + " Received request for " + request.url);
+  response.writeHead(404);
+  response.end();
 });
 
-const wss = new WebSocketServer({ server });
+server;
 
-let userCount = 0;
-
-wss.on("connection", function connection(ws) {
-  ws.on("error", console.error);
-
-  ws.on("message", function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
-  });
-
-  console.log("User connected", ++userCount);
-  ws.send("Hello from WebSocket");
-});
+const userManager = new UserManager();
+const store = new InMemoryStore();
 
 server.listen(8080, function () {
-  console.log(new Date() + "Server is listening on 8080");
+  console.log(new Date() + " Server is listening on port 8080");
 });
+
+const wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false,
+});
+
+function originIsAllowed(origin: string) {
+  return true;
+}
+
+wsServer.on("request", function (request) {
+  console.log("inside connect");
+
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    console.log(
+      new Date() + " Connection from origin " + request.origin + " rejected."
+    );
+    return;
+  }
+
+  var connection = request.accept("echo-protocol", request.origin);
+  console.log(new Date() + " Connection accepted.");
+  connection.on("message", function (message) {
+    // Todo add rate limitting logic here
+    if (message.type === "utf8") {
+      try {
+        messageHandler(connection, JSON.parse(message.utf8Data));
+      } catch (e) {}
+    }
+  });
+});
+
+function messageHandler(ws: connection, message: IncomingMessage) {
+  if (message.type == SupportedMessage.JoinRoom) {
+    const payload = message.payload;
+    userManager.addUser(payload.name, payload.userId, payload.roomId, ws);
+  }
+
+  if (message.type === SupportedMessage.SendMessage) {
+    const payload = message.payload;
+    const user = userManager.getUser(payload.roomId, payload.userId);
+
+    if (!user) {
+      console.error("User not found in the db");
+      return;
+    }
+    let chat = store.addChat(
+      payload.userId,
+      user.name,
+      payload.roomId,
+      payload.message
+    );
+    if (!chat) {
+      return;
+    }
+
+    const outgoingPayload: OutgoingMessage = {
+      type: OutgoingSupportedMessages.AddChat,
+      payload: {
+        chatId: chat.id,
+        roomId: payload.roomId,
+        message: payload.message,
+        name: user.name,
+        upvotes: 0,
+      },
+    };
+    userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+  }
+
+  if (message.type === SupportedMessage.UpvoteMessage) {
+    const payload = message.payload;
+    const chat = store.upvote(payload.userId, payload.roomId, payload.chatId);
+    console.log("inside upvote");
+    if (!chat) {
+      return;
+    }
+    console.log("inside upvote 2");
+
+    const outgoingPayload: OutgoingMessage = {
+      type: OutgoingSupportedMessages.UpdateChat,
+      payload: {
+        chatId: payload.chatId,
+        roomId: payload.roomId,
+        upvotes: chat.upvotes.length,
+      },
+    };
+
+    console.log("inside upvote 3");
+    userManager.broadcast(payload.roomId, payload.userId, outgoingPayload);
+  }
+}
